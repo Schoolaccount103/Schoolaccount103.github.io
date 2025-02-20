bc7d08d50ccd280ea39cdb4a1b0144197525fbb0
Schoolaccount103 <153403289+Schoolaccount103@users.noreply.github.com>
2025-02-20 13:06:15 -0500

    Update and rename placeholder to index.php

---
+++ web250/php/index.php
@@ -0,0 +1,8 @@
+<?php 
+$pageTitle = "Homepage for Sigmas";
+include('header.php'); 
+?>
+<div class="rounded-rectangle">
+    <p>Welcome, I'm excited to share my journey as a student at CPCC. Here, you'll find information about my classes, projects, and interests. I encourage you to explore the sections and learn more about me in my <a href="introduction.php">introduction.</a> This site showcases what I am currently working on in WEB250.</p>
+</div>
+<?php include('footer.php'); ?>
