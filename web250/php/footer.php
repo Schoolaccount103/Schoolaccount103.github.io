c20fed444be26b21ff7cab7fdf3733fdc53a8ad3
Schoolaccount103 <153403289+Schoolaccount103@users.noreply.github.com>
2025-02-22 11:13:33 -0500

    Add files via upload

---
+++ web250/php/footer.php
@@ -0,0 +1,13 @@
+<footer>
+        <ul class="col1l">
+            <li class="coll"><a href="https://github.com/Schoolaccount103">GitHub</a></li>
+            <li class="coll"><a href="https://schoolaccount103.github.io/">GitHub.io</a></li>
+            <li class="coll"><a href="https://schoolaccount103.github.io/web250/">WEB250.io</a></li>
+            <li class="coll"><a href="https://www.freecodecamp.org/currently">freeCodeCamp</a></li>
+            <li class="coll"><a href="https://www.codecademy.com/profiles/currentlyslacking">Codeacademy</a></li>
+            <li class="coll"><a href="https://jsfiddle.net/user/CurrentlySlacking">JSFiddle</a></li>
+            <li class="coll"><a href="https://www.linkedin.com/in/lee-harris-697a3b2a9/">Linkedin</a></li>
+        </ul>
+    </footer>
+</body>
+</html>
---
+++ web250/php/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle; ?></title>
+    <link rel="stylesheet" href="styles/default.css">
+</head>
+<body>
+
+    <header class="header">
+        <h1>Lee Harris's Leaping Horse || Web250</h1>
+        <nav>
+            <ul class="col1l">
+                <?php 
+                $navItems = [
+                    'Home' => 'index.php',
+                    'Introduction' => 'introduction.php',
+                    'Contract' => 'contract.php'
+                ];
+                foreach ($navItems as $name => $link) {
+                    echo "<li class='coll'><a href='$link'>$name</a></li>";
+                }
+                ?>
+                <li class="coll"><a href="htdocs/web250/index.html">View Static Site</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
