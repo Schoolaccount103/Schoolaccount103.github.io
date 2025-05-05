f1f243501013476ded7665103e539ee4a056162c
Schoolaccount103 <153403289+Schoolaccount103@users.noreply.github.com>
2025-05-05 19:36:53 -0400

    Update include.js

--- web250/scripts/include.js
+++ web250/scripts/include.js
@@ -1,1 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
 
+    fetch('components/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML('afterbegin', data);
+        });
+
+    fetch('components/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML('beforeend', data);
+        });
+});
